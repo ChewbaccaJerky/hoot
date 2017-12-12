9865bf792737f0d11091ad5147401a32366c12c5
John Robin <johnrobinlugtu@gmail.com>
2017-12-12 14:16:25 -0800

    Added Stars information

--- frontend/components/rating/ratings.jsx
+++ frontend/components/rating/ratings.jsx
@@ -19,26 +19,3 @@ const Rating = (props) => {
 };
 
 export default Rating;
-
-/* star images
-
-blue star
-http://res.cloudinary.com/nearbytes/image/upload/q_100/v1495210337/good-star_g0lskg.png
-
-orange star
-http://www.freepngimg.com/download/star/11-star-png-image.png
-
-yellow star
-http://www.freepngimg.com/download/star/8-yellow-star-png-image.png
-*/
-
-// content = (
-//   <ReactStars
-//     className="ratings"
-//     count={5}
-//     size={24}
-//     color1={"#E8E8E8"}
-//     color2={"#f8cc14"}
-//     value={props.ratings}
-//     edit={false}/>
-// );
